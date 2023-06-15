5710604abd0637c3d2058b0bcb8b3b7980cb1faf
Ansh Shukla <contact@anshshukla.dev>
2023-06-14 19:44:30 -0700

    init prettier

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,11 @@
 export function App() {
   return (
-    <>
+    <div class="flex flex-col h-screen">
       <header>
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
       </header>
-      <main>Main</main>
+      <main class="mb-auto">Main</main>
       <footer>Footer</footer>
-    </>
-  )
+    </div>
+  );
 }
