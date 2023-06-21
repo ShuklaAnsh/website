c889f96da431818447d57c083e1d6c39589bddba
Ansh Shukla <contact@anshshukla.dev>
2023-06-20 20:57:19 -0700

    fix: broken blurb

--- src/components/blurb/blurb.tsx
+++ src/components/blurb/blurb.tsx
@@ -1,18 +1,23 @@
-import { PrismAsyncLight as SyntaxHighlighter } from "react-syntax-highlighter";
-import {
-  oneDark as darkStyle,
-  oneLight as lightStyle,
-} from "react-syntax-highlighter/dist/esm/styles/prism";
+import { lazy, Suspense } from "react";
+import darkStyle from "react-syntax-highlighter/dist/cjs/styles/hljs/atom-one-dark-reasonable";
+import lightStyle from "react-syntax-highlighter/dist/cjs/styles/hljs/atom-one-light";
 import { blurbTxt } from "./blurb-txt";
 import { useIsDarkMode } from "../../hooks";
 
 export const Blurb = () => {
+  const SyntaxHighlighter = lazy(
+    () => import("react-syntax-highlighter/dist/esm/light-async")
+  );
   const isDarkMode = useIsDarkMode();
   const style = isDarkMode ? darkStyle : lightStyle;
 
   return (
-    <SyntaxHighlighter language="markup" style={style}>
-      {blurbTxt}
-    </SyntaxHighlighter>
+    <Suspense>
+      <SyntaxHighlighter language="xml" style={style}>
+        {blurbTxt}
+      </SyntaxHighlighter>
+    </Suspense>
   );
 };
+
+export default Blurb;
