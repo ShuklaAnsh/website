d7ca377da87acbf311fcea0f6696e03365ed3798
Ansh Shukla <contact@anshshukla.dev>
2023-07-05 03:50:20 -0700

    feat: tweak gallery expand bahaviour

--- src/components/gallery/gallery.tsx
+++ src/components/gallery/gallery.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { Card } from "..";
 import { GalleryItem } from "./gallery-item";
 import { GalleryExpandButton } from "./gallery-expand-btn";
@@ -17,17 +17,31 @@ export const Gallery: React.FC<IGalleryProps> & IGallerySubComponents = (
   props
 ) => {
   const [expanded, setExpanded] = useState(false);
-  const toggleExpanded = () => setExpanded((curr) => !curr);
+  const cardRef = useRef<HTMLElement>(null);
+
+  const toggleExpanded = () => {
+    setExpanded(!expanded);
+    cardRef.current?.scrollIntoView({
+      behavior: expanded ? "instant" : "smooth",
+    });
+  };
 
   return (
-    <Card className="bg-zinc-300 dark:bg-zinc-900">
-      <Card.Header className="flex">
+    <Card
+      ref={cardRef}
+      className="scroll-mt-10 bg-zinc-300  dark:bg-zinc-900 md:scroll-mt-14"
+    >
+      <Card.Header
+        className={`${
+          expanded ? "sticky top-10 md:top-14" : ""
+        } flex bg-zinc-300 dark:bg-zinc-900 `}
+      >
         <h3 className="flex-1">{props.header}</h3>
         <GalleryExpandButton isExpanded={expanded} onClick={toggleExpanded} />
       </Card.Header>
       <Card.Content
         className={`overflow-auto transition-[max-height] duration-700 ${
-          expanded ? "max-h-[9999px]" : "max-h-[calc(100vh/2)]"
+          expanded ? "max-h-[9999px]" : "max-h-[calc(100vh/3)]"
         } `}
       >
         <div class={`not-prose grid grid-cols-2 gap-4`}>{props.children}</div>
