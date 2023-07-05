88495d45c12984a17b8e2c8d92a1384ffcb37c3e
Ansh Shukla <contact@anshshukla.dev>
2023-07-05 03:50:02 -0700

    chore: cleanup topbar styling

--- src/components/topbar/topbar.tsx
+++ src/components/topbar/topbar.tsx
@@ -3,20 +3,25 @@ import { useWindowScroll } from "../../hooks";
 export const Topbar = () => {
   const scrollTop = useWindowScroll({ updateLimit: 200, steps: 5 });
 
-  let bgClass = "sticky top-0 transition-all duration-200";
-  let textClass =
-    "container text-center mx-auto p-2 md:p-4 text-3xl font-bold transition-colors duration-300";
+  let bgClass = "";
+  let textClass = "";
   if (scrollTop <= 15) {
-    bgClass += " bg-transparent";
-    textClass += " text-zinc-200";
+    bgClass = "bg-transparent";
+    textClass += "text-zinc-200";
   } else {
-    bgClass += " bg-zinc-200 dark:bg-zinc-900 shadow-md";
-    textClass += " text-verdant-green";
+    bgClass = "bg-zinc-200 dark:bg-zinc-900 shadow-md";
+    textClass += "text-verdant-green";
   }
 
   return (
-    <header className={bgClass}>
-      <nav className={textClass}>Ansh Shukla</nav>
+    <header
+      className={`sticky top-0 z-50 transition-all duration-200 ${bgClass}`}
+    >
+      <nav
+        className={`container mx-auto p-2 text-center text-3xl font-bold transition-colors duration-300 md:p-4 ${textClass}`}
+      >
+        Ansh Shukla
+      </nav>
     </header>
   );
 };
