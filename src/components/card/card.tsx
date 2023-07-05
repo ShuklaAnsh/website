48da9bdc362503b5eb08f6edcca2a3a5e2e21943
Ansh Shukla <contact@anshshukla.dev>
2023-07-05 03:47:19 -0700

    feat: card forwarded ref

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,27 +1,25 @@
-import { CardFooter, ICardFooterProps } from "./card-footer";
-import { CardHeader, ICardHeaderProps } from "./card-header";
-import { CardContent, ICardContentProps } from "./card-content";
+import { forwardRef } from "react";
+import { CardHeader } from "./card-header";
+import { CardFooter } from "./card-footer";
+import { CardContent } from "./card-content";
 
 interface ICardProps extends React.PropsWithChildren {
   className?: string;
 }
 
-interface CardSubComponents {
-  Header: React.FC<ICardHeaderProps>;
-  Content: React.FC<ICardContentProps>;
-  Footer: React.FC<ICardFooterProps>;
-}
-
-export const Card: React.FC<ICardProps> & CardSubComponents = (props) => {
+const CardComponent = forwardRef<HTMLElement, ICardProps>((props, ref) => {
   return (
     <article
+      ref={ref}
       className={`prose prose-zinc mx-auto w-full rounded-xl bg-zinc-200 shadow dark:prose-invert dark:bg-zinc-800 ${props.className}`}
     >
       {props.children}
     </article>
   );
-};
+});
 
-Card.Header = CardHeader;
-Card.Content = CardContent;
-Card.Footer = CardFooter;
+export const Card = Object.assign(CardComponent, {
+  Header: CardHeader,
+  Content: CardContent,
+  Footer: CardFooter,
+});
